64f7412a33bdcde45265d6ef43c205dc3c54b2ee
MarcelLeal <marcel@pgentulhos.com.br>
2025-10-23 11:23:00 -0300

    Sum

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,9 +1,3 @@
 export const Sum = ({ a = 0, b = 0 }) => {
-  const total = a + b;
-
-  return (
-    <p>
-      Sum of {a} and {b} is {total}
-    </p>
-  );
+  return <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>;
 };
